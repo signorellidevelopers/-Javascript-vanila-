01b7630c4a9ec770e9ae5f3f474c23ae8578619b
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2020-09-11 20:26:10 -0300

    fim dom int object

---
+++ dom/exe007Dom/script.js
@@ -0,0 +1,10 @@
+(function () {
+  "use strict";
+  let paragrafo = document.querySelectorAll(".paragrafo-div");
+  var x = 0;
+  while (paragrafo[x]) {
+    paragrafo[x].textContent = x + "-site hackeda kkkkk";
+
+    x++;
+  }
+})();
