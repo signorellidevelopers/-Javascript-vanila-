0b1b402fd66997f5069b50e59b8ae42371601ceb
signorellidevelopers <66884792+signorellidevelopers@users.noreply.github.com>
2020-09-12 11:17:53 -0300

    senguindo com object

--- object/introduçao-objeto/script.js
+++ object/introduçao-objeto/script.js
@@ -1,1 +1,21 @@
-let minhaStr = "mario";
+hoje = new Date();
+let x = [0];
+function mudaX(x) {
+  let y = x;
+  y.push(2);
+  console.log("mudaX");
+  console.log(y);
+  console.log(x);
+}
+mudaX(x);
+console.log("x:");
+console.log(x);
+console.log("--------");
+
+let caneta1 = {
+  cor: "red",
+  mudaCor: function (cor) {
+    debugger;
+    this.cor = cor;
+  },
+};
